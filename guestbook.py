deaf52e7314fda15fcb743b4378a4790b39e7425
mira67 <qliu.hit@gmail.com>
2015-03-28 16:39:38 -0600

    new layout in

--- guestbook.py
+++ guestbook.py
@@ -9,6 +9,19 @@ import cloudstorage as gcs
 import jinja2
 import webapp2
 
+import pprint
+import logging
+
+import StringIO
+import subprocess
+def no_popen(*args, **kwargs): raise OSError("forbjudet")
+subprocess.Popen = no_popen  # not allowed in GAE, missing from module
+subprocess.PIPE = None
+subprocess.STDOUT = None
+os.environ["MATPLOTLIBDATA"] = os.getcwdu()  # own matplotlib data
+os.environ["MPLCONFIGDIR"] = os.getcwdu()    # own matplotlibrc
+import numpy, matplotlib, matplotlib.pyplot as plt
+
 from google.appengine.api import app_identity
 
 my_default_retry_params = gcs.RetryParams(initial_delay=0.2,
@@ -51,6 +64,26 @@ class Greeting(ndb.Model):
     content = ndb.StringProperty(indexed=False)
     date = ndb.DateTimeProperty(auto_now_add=True)
 
+def dynamic_png():
+    try:
+        plt.title("Dynamic PNG")
+        for i in range(5): plt.plot(sorted(numpy.random.randn(25)))
+        rv = StringIO.StringIO()
+        plt.savefig(rv, format="png")
+        plt.clf()
+        return """<img src="data:image/png;base64,%s"/>""" % rv.getvalue().encode("base64").strip()
+    finally:
+        plt.clf()
+
+def dynamic_svg():
+    try:
+        plt.title("Dynamic SVG")
+        for i in range(5): plt.plot(sorted(numpy.random.randn(25)))
+        rv = StringIO.StringIO()
+        plt.savefig(rv, format="svg")
+        return rv.getvalue()
+    finally:
+        plt.clf()
 
 # [START main_page]
 class MainPage(webapp2.RequestHandler):
@@ -74,15 +107,16 @@ class MainPage(webapp2.RequestHandler):
                      #            app_identity.get_default_gcs_bucket_name())
         bucket = '/' + bucket_name
         filename = bucket + '/nt_197810_n07_v01_s.bin'
-
+        #testimg = dynamic_png()
 
         template_values = {
-            'user': user,
-            'greetings': greetings,
-            'guestbook_name': urllib.quote_plus(guestbook_name),
-            'url': url,
-            'url_linktext': url_linktext,
-            'teststring': filename,
+      #      'user': user,
+       #     'greetings': greetings,
+        #    'guestbook_name': urllib.quote_plus(guestbook_name),
+         #   'url': url,
+          #  'url_linktext': url_linktext,
+           # 'teststring': filename,
+        #    'testimage': testimg,
         }
 
         template = JINJA_ENVIRONMENT.get_template('index.html')
