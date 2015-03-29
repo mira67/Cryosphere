6df56057b3dea7d65020db3b1e9be6e069294947
mira67 <qliu.hit@gmail.com>
2015-03-28 22:40:59 -0600

    web form value request in

--- guestbook.py
+++ guestbook.py
@@ -12,16 +12,6 @@ import webapp2
 import pprint
 import logging
 
-import StringIO
-import subprocess
-def no_popen(*args, **kwargs): raise OSError("forbjudet")
-subprocess.Popen = no_popen  # not allowed in GAE, missing from module
-subprocess.PIPE = None
-subprocess.STDOUT = None
-os.environ["MATPLOTLIBDATA"] = os.getcwdu()  # own matplotlib data
-os.environ["MPLCONFIGDIR"] = os.getcwdu()    # own matplotlibrc
-import numpy, matplotlib, matplotlib.pyplot as plt
-
 from google.appengine.api import app_identity
 
 my_default_retry_params = gcs.RetryParams(initial_delay=0.2,
@@ -39,116 +29,25 @@ JINJA_ENVIRONMENT = jinja2.Environment(
 DEFAULT_GUESTBOOK_NAME = 'default_guestbook'
 
 
-# We set a parent key on the 'Greetings' to ensure that they are all
-# in the same entity group. Queries across the single entity group
-# will be consistent. However, the write rate should be limited to
-# ~1/second.
-
-def guestbook_key(guestbook_name=DEFAULT_GUESTBOOK_NAME):
-    """Constructs a Datastore key for a Guestbook entity.
-
-    We use guestbook_name as the key.
-    """
-    return ndb.Key('Guestbook', guestbook_name)
-
-
-class Author(ndb.Model):
-    """Sub model for representing an author."""
-    identity = ndb.StringProperty(indexed=False)
-    email = ndb.StringProperty(indexed=False)
-
-
-class Greeting(ndb.Model):
-    """A main model for representing an individual Guestbook entry."""
-    author = ndb.StructuredProperty(Author)
-    content = ndb.StringProperty(indexed=False)
-    date = ndb.DateTimeProperty(auto_now_add=True)
-
-def dynamic_png():
-    try:
-        plt.title("Dynamic PNG")
-        for i in range(5): plt.plot(sorted(numpy.random.randn(25)))
-        rv = StringIO.StringIO()
-        plt.savefig(rv, format="png")
-        plt.clf()
-        return """<img src="data:image/png;base64,%s"/>""" % rv.getvalue().encode("base64").strip()
-    finally:
-        plt.clf()
-
-def dynamic_svg():
-    try:
-        plt.title("Dynamic SVG")
-        for i in range(5): plt.plot(sorted(numpy.random.randn(25)))
-        rv = StringIO.StringIO()
-        plt.savefig(rv, format="svg")
-        return rv.getvalue()
-    finally:
-        plt.clf()
-
 # [START main_page]
 class MainPage(webapp2.RequestHandler):
 
     def get(self):
-        guestbook_name = self.request.get('guestbook_name',
-                                          DEFAULT_GUESTBOOK_NAME)
-        greetings_query = Greeting.query(
-            ancestor=guestbook_key(guestbook_name)).order(-Greeting.date)
-        greetings = greetings_query.fetch(10)
 
-        user = users.get_current_user()
-        if user:
-            url = users.create_logout_url(self.request.uri)
-            url_linktext = 'Logout'
-        else:
-            url = users.create_login_url(self.request.uri)
-            url_linktext = 'Login'
-
-        bucket_name = 'testcat'#os.environ.get('BUCKET_NAME',
-                     #            app_identity.get_default_gcs_bucket_name())
-        bucket = '/' + bucket_name
-        filename = bucket + '/nt_197810_n07_v01_s.bin'
-        #testimg = dynamic_png()
-
-        template_values = {
-      #      'user': user,
-       #     'greetings': greetings,
-        #    'guestbook_name': urllib.quote_plus(guestbook_name),
-         #   'url': url,
-          #  'url_linktext': url_linktext,
-           # 'teststring': filename,
-        #    'testimage': testimg,
-        }
+        template_values ={}
 
         template = JINJA_ENVIRONMENT.get_template('index.html')
         self.response.write(template.render(template_values))
 # [END main_page]
 
+class Query(webapp2.RequestHandler):
 
-class Guestbook(webapp2.RequestHandler):
-
-    def post(self):
-        # We set the same parent key on the 'Greeting' to ensure each
-        # Greeting is in the same entity group. Queries across the
-        # single entity group will be consistent. However, the write
-        # rate to a single entity group should be limited to
-        # ~1/second.
-        guestbook_name = self.request.get('guestbook_name',
-                                          DEFAULT_GUESTBOOK_NAME)
-        greeting = Greeting(parent=guestbook_key(guestbook_name))
-
-        if users.get_current_user():
-            greeting.author = Author(
-                    identity=users.get_current_user().user_id(),
-                    email=users.get_current_user().email())
-
-        greeting.content = self.request.get('content')
-        greeting.put()
-
-        query_params = {'guestbook_name': guestbook_name}
-        self.redirect('/?' + urllib.urlencode(query_params))
-
+    def get(self):
+        user_query = self.request.get('query')
+        #get query and test
+        self.response.out.write(user_query)
 
 application = webapp2.WSGIApplication([
     ('/', MainPage),
-    ('/sign', Guestbook),
+    ('/user_query', Query),
 ], debug=True)
