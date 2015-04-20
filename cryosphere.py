707e4d288e04504910481097ae6ae81ec4d474fd
mira67 <qliu.hit@gmail.com>
2015-04-19 23:31:11 -0600

    whole raw process flow works;need fix request timeour bug

--- cryosphere.py
+++ cryosphere.py
@@ -11,8 +11,10 @@ import webapp2
 
 import pprint
 import logging
+import time
 
 from google.appengine.api import app_identity
+from query_process import QueryProc
 
 my_default_retry_params = gcs.RetryParams(initial_delay=0.2,
                                           max_delay=5.0,
@@ -54,13 +56,27 @@ class Query(webapp2.RequestHandler):
         user_query_c = user_query.upper()
         #QUERY for MAX MIN EXTENT, result should be the day, and show image
         if user_query_c == 'MAX MIN EXTENT':
-        #call the function
+            #call processing module
+            query_process = QueryProc()
+
+            start_time = time.time()
+            query_results = query_process.maxmin_proc_raw()
+            query_raw_time = time.time() - start_time                        
+            #query_results = query_process.maxmin_proc_cmp()
+
+            #store image and just show, no interactive as first version
+
+            
+            #call the function, need wrap this update into a function
             template_values = {
-            'console': user_query_c,
+            'user_input': "User's Query: " + user_query_c,
+            'query_result': query_results,
+            'proc_time': "Query process time: %s" % query_raw_time,
             }
 
             template = JINJA_ENVIRONMENT.get_template('index.html')
             self.response.write(template.render(template_values)) 
+
         #QUERY for Anomaly analysis, show the trend and std
         elif user_query_c == 'SEA ICE EXTENT ANOMALY':
             self.response.write("here is the query %s" % user_query_c) 
---
+++ query_process.py
@@ -0,0 +1,84 @@
+# [START imports]
+import os
+from google.appengine.ext import ndb
+import cloudstorage as gcs
+import numpy as np
+import array
+import math
+import sys
+
+my_default_retry_params = gcs.RetryParams(initial_delay=0.2,
+                                          max_delay=5.0,
+                                          backoff_factor=2,
+                                          max_retry_period=15)
+gcs.set_default_retry_params(my_default_retry_params)
+
+#default data path
+BUCKET = '/nsidc-0051'
+MONTH_RP = '/monthly'
+DAILY_RP = '/daily'
+MONTH_CP = '/monthly_cmp'
+DAILY_CP = '/daily_cmp'
+
+class QueryProc(object):
+
+    def blk_total_seaice(self,blk):
+        thr1 = 250
+        thr2 = 37.5
+        cell_size = 25#km
+        seaice_area = len(blk[np.where(blk<=thr1)]) -  len(blk[np.where(blk<thr2)])
+        seaice_area = math.floor(10*seaice_area*25.0*25.0/1000)#km^2,scaled by 10
+        return seaice_area
+
+    def maxmin_proc_raw(self):
+        #get files list from gs
+        objs = gcs.listbucket(BUCKET+MONTH_RP)
+        filelist = []
+        for obj in objs:
+            filelist.append(obj.filename)
+        #go through and calculate
+        max_seaice = 0
+        min_seaice = float('inf')
+        result = []
+        i = 0
+        max_id = 0
+        min_id = 0
+        gcs_file = []
+        raw_data = []
+        for file in filelist:
+            i = i + 1
+            filename = file
+            #raw_data = np.fromfile(filename, dtype=np.uint8, sep='')
+            gcs_file = gcs.open(filename,mode='r')
+            # Read the data into an array
+            raw_file = gcs_file.read()
+            raw_data = array.array('B',raw_file)
+            raw_data = np.array(raw_data[300:])#offset 300 pixels
+            raw_data = raw_data.reshape(316,332);
+            total_seaice = self.blk_total_seaice(raw_data)
+            if total_seaice >= max_seaice:
+                max_id = i-1
+                max_seaice = total_seaice
+                min_seaice = max_seaice
+            if total_seaice <= min_seaice:
+                min_id = i-1
+                min_seaice = total_seaice
+
+            gcs_file.close()
+            if i > 5:
+                break
+
+        result.append(max_seaice)
+        result.append(max_id)
+        result.append(min_seaice)
+        result.append(min_id)
+        #debugging
+        return result 
+
+    def maxmin_proc_cmp(self):
+        pass
+        return results #a list or dict
+
+
+
+
